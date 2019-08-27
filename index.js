5cbec56f6f143ab409d01b831318a74f6bad1e1c
ClaudiaQuezadaRiquelme <cklaho.quesoda.X@gmail.com>
2019-08-27 15:44:53 -0400

    dos librerias. Probando encontrar .md y encontrar links en cada .md

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 module.exports = () => {
   // ...
 };
+
+const FileHound = require('filehound');
+let fs = require('fs');
+let markdownLinkExtractor = require('markdown-link-extractor');
+ 
+const files = FileHound.create()
+  .paths('./')
+  .ext('md')
+  .find();
+ 
+files.then(res => {
+  res.forEach( (element)=> {
+    console.log('filehound: '+element)
+    let markdown = fs.readFileSync(element).toString();
+    let links = markdownLinkExtractor(markdown);
+    links.forEach(function (link) {
+      console.log('markdown-link-extractor: '+ link);
+      });
+    
+  })
+  
+});
+
+
+
+
+
+
+
+
+

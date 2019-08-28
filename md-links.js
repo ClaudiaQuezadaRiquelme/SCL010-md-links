63638d58bc35061b7f9ae7f6c360a51c644b6a29
ClaudiaQuezadaRiquelme <cklaho.quesoda.X@gmail.com>
2019-08-27 21:25:04 -0400

    se ejecuta app en la terminal con process.stdin

---
+++ md-links.js
@@ -0,0 +1,51 @@
+// Get process.stdin as the standard input object.
+let standardInput = process.stdin;
+
+// Set input character encoding.
+standardInput.setEncoding('utf-8');
+
+// Prompt user to input data in console.
+console.log('Please input a directory in command line, "exit" to close.');
+
+// When user input data and click enter key.
+standardInput.on('data', function (data) {
+
+    // User input exit.
+    if(data === 'exit\n'){
+        // Program exit.
+        console.log("User input complete, program exit.");
+        process.exit();
+    }else
+    {
+        // Print user input in console.
+        console.log('User Input Data : ' + data);
+        let directory = data.replace('\n', '');
+        findUrlAndLinks(directory);
+    }
+});
+
+//esto tiene que ser una función y adentro de paths tiene que ingresar data
+const findUrlAndLinks = (Directory) => {
+    const FileHound = require('filehound');
+    let fs = require('fs');
+    let markdownLinkExtractor = require('markdown-link-extractor');
+    
+    const files = FileHound.create()
+    .paths(Directory)
+    .ext('md')
+    .find();
+    
+    files.then(res => {
+        res.forEach( (element) => {
+            console.log('filehound: '+element)
+            let markdown = fs.readFileSync(element).toString();
+            let links = markdownLinkExtractor(markdown);
+            links.forEach(function (link) {
+            console.log('markdown-link-extractor: '+ link);
+            });
+        })
+    });
+}
+
+
+

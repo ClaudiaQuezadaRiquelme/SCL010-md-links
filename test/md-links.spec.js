7a21993f9ec7dabe7aaf2f85b1d66055682e9b7b
ClaudiaQuezadaRiquelme <cklaho.quesoda.X@gmail.com>
2019-09-03 22:00:06 -0400

    test for callMarkdownLinkExtractor and returnTruncatedText

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,22 @@
-const mdLinks = require('../');
+const mdLinks = require('../md-links.js');
 
 
-describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
-  });
-
+describe('mdLinks es un objeto cuyos atributos son funciones', () => {
+  describe('el atributo "callMarkdownLinkExtractor" es una función que retorna un arreglo de strings', () => {
+    it('debería ser una objeto', () => {
+      expect(typeof mdLinks.callMarkdownLinkExtractor('./md-files/readme-e.md',undefined)).toBe( 'object' );
+    });
+    it('debería ser una string', () => {
+      expect(typeof mdLinks.callMarkdownLinkExtractor('./md-files/readme-e.md',undefined)[0]).toBe( 'string' );
+    });
+  }),
+  describe('el atributo "returnTruncatedText" es una función que retorna un string truncado', () => {
+    it('debería ser una string', () => {
+      expect(typeof mdLinks.returnTruncatedText('HOLIHOLIHoliholiOLIOLIholiOLIholiHILIlilooLIHILihoLOAIliadsdASDFASDFASDFASDFASDFASDFASDFSADFSADFASDFSDFASDFDSFASDFASDFSDFSADFSDFASDFSDFSDFSADFASDFSDFSDFASDFASDFASDFASDFSADFSADFASDFASDFASDFASDFSADFSADFASDFSADFSADFASDFSADFadfsdfsdfasdfasdfasdfasdfasdfsdfasdfasdfasdfasdfasdfasdfsadfsadfsadfsadfsdafsadfsdafasdfasdfasdfsadfsadfsadfsadfsadfasdfsadfsadfasdfasdfsadfasdfsdafsdfjkafgfgjfjfgasdfhjgsdafjghasdfkhgsadfasdjghasdfjhgsdfajhgasdfasdhgasdfasjhgsadfasdfhgsadfasjghasdfaghasdfjhgsadfjhgsdfasdfjhgasdfasjdhgasdfajsdgasdfsadhjgasdfasdjhgasdfsadfjhgsdafadsfjhgasdfsdfhjgsadfasdfjhgsadfjafgsdajghasdfjkgadsfjhgasdfjgaskdfjhgasdfjadsfjsdafasdfjhfjasksdahasdjfgkjfgjfgfgjfgjhfgjhfgjhfgfgjhfgjfgjhfgjhfgjhfgjgfjhfgjfgasdfasdfasdfasdfasdfasdfasdfasdfjhgkjhgkjhgkjhgkjhgjhgkhgjkhgkjhgasdfasdfsadfasdfasdfasdfasdfasdfasdfjhgkjhgjhgjkhgjhgjkgjhgjhgjkhgkjgadsfasdfasdfasdfasdfasdfasdfasdfasdfjhgjhgjhggjhgkjhgkjhgjhggadfsasdfasdfasdfas')).toBe( 'string' );
+    });
+    it('debería retornar "HOLIHOLIHoliholiOLIOLIholiOLIholiHILIlilooLIHILiho"', () => {
+      expect(mdLinks.returnTruncatedText('HOLIHOLIHoliholiOLIOLIholiOLIholiHILIlilooLIHILihoLOAIliadsdASDFASDFASDFASDFASDFASDFASDFSADFSADFASDFSDFASDFDSFASDFASDFSDFSADFSDFASDFSDFSDFSADFASDFSDFSDFASDFASDFASDFASDFSADFSADFASDFASDFASDFASDFSADFSADFASDFSADFSADFASDFSADFadfsdfsdfasdfasdfasdfasdfasdfsdfasdfasdfasdfasdfasdfasdfsadfsadfsadfsadfsdafsadfsdafasdfasdfasdfsadfsadfsadfsadfsadfasdfsadfsadfasdfasdfsadfasdfsdafsdfjkafgfgjfjfgasdfhjgsdafjghasdfkhgsadfasdjghasdfjhgsdfajhgasdfasdhgasdfasjhgsadfasdfhgsadfasjghasdfaghasdfjhgsadfjhgsdfasdfjhgasdfasjdhgasdfajsdgasdfsadhjgasdfasdjhgasdfsadfjhgsdafadsfjhgasdfsdfhjgsadfasdfjhgsadfjafgsdajghasdfjkgadsfjhgasdfjgaskdfjhgasdfjadsfjsdafasdfjhfjasksdahasdjfgkjfgjfgfgjfgjhfgjhfgjhfgfgjhfgjfgjhfgjhfgjhfgjgfjhfgjfgasdfasdfasdfasdfasdfasdfasdfasdfjhgkjhgkjhgkjhgkjhgjhgkhgjkhgkjhgasdfasdfsadfasdfasdfasdfasdfasdfasdfjhgkjhgjhgjkhgjhgjkgjhgjhgjkhgkjgadsfasdfasdfasdfasdfasdfasdfasdfasdfjhgjhgjhggjhgkjhgkjhgjhggadfsasdfasdfasdfas')).toBe( 'HOLIHOLIHoliholiOLIOLIholiOLIholiHILIlilooLIHILiho' );
+    });
+  })
+    
 });

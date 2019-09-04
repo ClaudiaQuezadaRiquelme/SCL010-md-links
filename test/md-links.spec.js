dcfed713fabfe6defc4f10ab7f7e559463e63e38
ClaudiaQuezadaRiquelme <cklaho.quesoda.X@gmail.com>
2019-09-03 22:33:40 -0400

    me volvi loca con los test

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -9,6 +9,24 @@ describe('mdLinks es un objeto cuyos atributos son funciones', () => {
     it('debería ser una string', () => {
       expect(typeof mdLinks.callMarkdownLinkExtractor('./md-files/readme-e.md',undefined)[0]).toBe( 'string' );
     });
+    describe('revisaremos lo retornado por el atributo "callMarkdownLinkExtractor"', () => {
+      it('return.split("  ") debería devolver arreglo', () => {
+        expect(typeof mdLinks.callMarkdownLinkExtractor('./md-files/readme-e.md',undefined)[0].split('  ')).toBe( 'object' );
+      });
+      it('return.split("  ") debería devolver arreglo con 3 string', () => {
+        expect(typeof mdLinks.callMarkdownLinkExtractor('./md-files/readme-e.md',undefined)[0].split('  ')[0]).toBe( 'string' );
+      });
+      it('return.split("  ") debería devolver arreglo con 3 string y el primer string debe ser el directorio esperado', () => {
+        expect(mdLinks.callMarkdownLinkExtractor('./md-files/readme-e.md',undefined)[0].split('  ')[0]).toBe( './md-files/readme-e.md' );
+      });
+      it('return.split("  ") debería devolver arreglo con 3 string y el segundo string debe ser el link esperado', () => {
+        expect(mdLinks.callMarkdownLinkExtractor('./md-files/readme-e.md',undefined)[0].split('  ')[1]).toBe( 'https://travis-ci.org/sindresorhus/array-union.svg?branch=master' );
+      });
+      it('return.split("  ") debería devolver arreglo con 3 string y el tercer string debe ser el string esperado', () => {
+        expect(mdLinks.callMarkdownLinkExtractor('./md-files/readme-e.md',undefined)[0].split('  ')[2]).toBe( 'HOLIHOLIHoliholiOLIOLIholiOLIholiHILIlilooLIHILiho' );
+      });
+    })
+    
   }),
   describe('el atributo "returnTruncatedText" es una función que retorna un string truncado', () => {
     it('debería ser una string', () => {
@@ -16,6 +34,9 @@ describe('mdLinks es un objeto cuyos atributos son funciones', () => {
     });
     it('debería retornar "HOLIHOLIHoliholiOLIOLIholiOLIholiHILIlilooLIHILiho"', () => {
       expect(mdLinks.returnTruncatedText('HOLIHOLIHoliholiOLIOLIholiOLIholiHILIlilooLIHILihoLOAIliadsdASDFASDFASDFASDFASDFASDFASDFSADFSADFASDFSDFASDFDSFASDFASDFSDFSADFSDFASDFSDFSDFSADFASDFSDFSDFASDFASDFASDFASDFSADFSADFASDFASDFASDFASDFSADFSADFASDFSADFSADFASDFSADFadfsdfsdfasdfasdfasdfasdfasdfsdfasdfasdfasdfasdfasdfasdfsadfsadfsadfsadfsdafsadfsdafasdfasdfasdfsadfsadfsadfsadfsadfasdfsadfsadfasdfasdfsadfasdfsdafsdfjkafgfgjfjfgasdfhjgsdafjghasdfkhgsadfasdjghasdfjhgsdfajhgasdfasdhgasdfasjhgsadfasdfhgsadfasjghasdfaghasdfjhgsadfjhgsdfasdfjhgasdfasjdhgasdfajsdgasdfsadhjgasdfasdjhgasdfsadfjhgsdafadsfjhgasdfsdfhjgsadfasdfjhgsadfjafgsdajghasdfjkgadsfjhgasdfjgaskdfjhgasdfjadsfjsdafasdfjhfjasksdahasdjfgkjfgjfgfgjfgjhfgjhfgjhfgfgjhfgjfgjhfgjhfgjhfgjgfjhfgjfgasdfasdfasdfasdfasdfasdfasdfasdfjhgkjhgkjhgkjhgkjhgjhgkhgjkhgkjhgasdfasdfsadfasdfasdfasdfasdfasdfasdfjhgkjhgjhgjkhgjhgjkgjhgjhgjkhgkjgadsfasdfasdfasdfasdfasdfasdfasdfasdfjhgjhgjhggjhgkjhgkjhgjhggadfsasdfasdfasdfas')).toBe( 'HOLIHOLIHoliholiOLIOLIholiOLIholiHILIlilooLIHILiho' );
+    });
+    it('el string retornado debería tener largo 50', () => {
+      expect(mdLinks.returnTruncatedText('HOLIHOLIHoliholiOLIOLIholiOLIholiHILIlilooLIHILihoLOAIliadsdASDFASDFASDFASDFASDFASDFASDFSADFSADFASDFSDFASDFDSFASDFASDFSDFSADFSDFASDFSDFSDFSADFASDFSDFSDFASDFASDFASDFASDFSADFSADFASDFASDFASDFASDFSADFSADFASDFSADFSADFASDFSADFadfsdfsdfasdfasdfasdfasdfasdfsdfasdfasdfasdfasdfasdfasdfsadfsadfsadfsadfsdafsadfsdafasdfasdfasdfsadfsadfsadfsadfsadfasdfsadfsadfasdfasdfsadfasdfsdafsdfjkafgfgjfjfgasdfhjgsdafjghasdfkhgsadfasdjghasdfjhgsdfajhgasdfasdhgasdfasjhgsadfasdfhgsadfasjghasdfaghasdfjhgsadfjhgsdfasdfjhgasdfasjdhgasdfajsdgasdfsadhjgasdfasdjhgasdfsadfjhgsdafadsfjhgasdfsdfhjgsadfasdfjhgsadfjafgsdajghasdfjkgadsfjhgasdfjgaskdfjhgasdfjadsfjsdafasdfjhfjasksdahasdjfgkjfgjfgfgjfgjhfgjhfgjhfgfgjhfgjfgjhfgjhfgjhfgjgfjhfgjfgasdfasdfasdfasdfasdfasdfasdfasdfjhgkjhgkjhgkjhgkjhgjhgkhgjkhgkjhgasdfasdfsadfasdfasdfasdfasdfasdfasdfjhgkjhgjhgjkhgjhgjkgjhgjhgjkhgkjgadsfasdfasdfasdfasdfasdfasdfasdfasdfjhgjhgjhggjhgkjhgkjhgjhggadfsasdfasdfasdfas').length).toBe( 50 );
     });
   })
     
